363efaf3b429a50919f1ed22a14e58e2dcfa2011
andytech580 <andytech580@gmail.com>
2026-03-28 11:51:26 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+import chalk from "chalk";
+
+global.owner = ["258833406646", "258871168283"];
+
+global.sessionName = "Sessions/Owner";
+global.version = "1.0";
+global.pairing_code = true;
+global.number_bot = "";
+global.prefa = ".";
+global.msgglobal =
+  "[𝑨𝑵𝑫𝒀-𝑿𝑴𝑫 𝑴𝑰𝑵𝑰] Welcome to the best WhatsApp bot experience!";
+globalThis.dev = "☄️ ANDY-TECH - Queen Mini Developer";
+
+global.api = {
+  url: "https://nex-magical.vercel.app",
+  url2: "https://api.vreden.my.id",
+  url3: "https://api-faa.my.id",
+};
+
+global.bot = {
+  name: "𝑨𝑵𝑫𝒀-𝑿𝑴𝑫 𝑴𝑰𝑵𝑰",
+  api: "https://nexevo.onrender.com",
+  web: "https://nexevo.onrender.com",
+  photo: "https://n.uguu.se/nQFWCVxU.jpg",
+  group: "https://whatsapp.com/channel/0029VbBhe8lCRs1fCxZ9OM3U",
+      channel: "https://whatsapp.com/channel/0029VbBhe8lCRs1fCxZ9OM3U",
+  newsletter: "120363421568891511",
+  github: "https://github.com/andytech580/ANDY-MINI",
+};
+
+global.mods = ["256701583113", "255740874777"];
+
+global.mess = {
+  socket: "🛡 This command can only be executed by the Socket.",
+  admin: "🛡 This command can only be executed by Group Admins.",
+  botAdmin: "🛡 This command can only be executed if the Socket is a Group Admin.",
+};
